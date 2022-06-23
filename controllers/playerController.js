e8add5c216a8c57af8c6d30ec3a3cf930aafeaf5
Nicholas Adi <nicholasadi22@gmail.com>
2022-06-23 22:59:18 +0700

    Revert "change nodemon to node"
    This reverts commit 0e04cda9116dc6c760712ecd56f710991e534e07.

--- controllers/playerController.js
+++ controllers/playerController.js
@@ -184,6 +184,8 @@ class PlayerController{
         };
         const secret = process.env.SECRET;
         const token = jwt.sign(payload,secret,{expiresIn: '1 hour', noTimestamp: true});
+        // res.cookie('token',token)
+        // console.log(payload)
         res.status(200).json({
           result: 'Success',
           data: payload,
